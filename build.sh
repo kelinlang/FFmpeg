13841116cdbcb03da437b6e7f82f9bdf789d80df
“kelinlang” <kelinlang@163.com>
2020-03-11 00:59:33 -0700

    build

--- build.sh
+++ build.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+./configure \
+	--enable-ffplay \
+	--enable-gpl \
+	--enable-libx264  \
+	--enable-libx265 \
+	--enable-static \
+	--enable-shared  \
+	--enable-debug  \
+	--disable-x86asm  \
+	#--enable-libsrt 
+	--extra-cflags=-g \
+	--extra-ldflags=-g \
+	--enable-libfdk-aac  \
+	--enable-nonfree
+	
+make clean
 make -j8
 sudo make install
 sudo rm /usr/local/lib/libav*
